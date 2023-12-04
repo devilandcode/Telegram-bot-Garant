5a1d78644ce008aa226622f69519b02dbb0c38a0
devilandcode <devilandcode@proton.me>
2023-12-04 15:26:42 +0300

    Add HomeController

--- kernel/Controller/Controller.php
+++ kernel/Controller/Controller.php
@@ -11,42 +11,42 @@ abstract class Controller
     )
     {
     }
-
-    public function start(): void
-    {
-        $this->botKeyboard->start();
-    }
-
-    public function profile(): void
-    {
-        $this->botAnswer->sendMyProfileData(
-            $this->parser->id_telegram,
-            $this->parser->username,
-            $this->cryptoApi->getBtcCurrency()->btcPrice,
-            $this->cryptoApi->getEthCurrency()->ethPrice,
-            $this->cryptoApi->getUSDTCurrency()->usdtPrice
-        );
-    }
-
-    public function search(): void
-    {
-        $this->botAnswer->askUserIdToSearch();
-    }
-
-    public function getMyDeals(): void
-    {
-        $this->botAnswer->activeDeals();
-    }
-
-    public function support(): void
-    {
-        $this->botAnswer->explainHowToUseBot();
-    }
-
-    public function unknownCommand(): void
-    {
-        $this->botAnswer->unknownCommand();
-    }
+//
+//    public function start(): void
+//    {
+//        $this->botKeyboard->start();
+//    }
+//
+//    public function profile(): void
+//    {
+//        $this->botAnswer->sendMyProfileData(
+//            $this->parser->id_telegram,
+//            $this->parser->username,
+//            $this->cryptoApi->getBtcCurrency()->btcPrice,
+//            $this->cryptoApi->getEthCurrency()->ethPrice,
+//            $this->cryptoApi->getUSDTCurrency()->usdtPrice
+//        );
+//    }
+//
+//    public function search(): void
+//    {
+//        $this->botAnswer->askUserIdToSearch();
+//    }
+//
+//    public function getMyDeals(): void
+//    {
+//        $this->botAnswer->activeDeals();
+//    }
+//
+//    public function support(): void
+//    {
+//        $this->botAnswer->explainHowToUseBot();
+//    }
+//
+//    public function unknownCommand(): void
+//    {
+//        $this->botAnswer->unknownCommand();
+//    }
 //
 //    public function sendCallBackAnswerToTelegram(): void
 //    {
--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -2,7 +2,44 @@
 
 namespace App\Controllers;
 
-class HomeController extends \App\Kernel\Controller\Controller
+class HomeController
 {
+
+
+    public function start(): void
+    {
+        $this->botKeyboard->start();
+    }
+
+    public function profile(): void
+    {
+        $this->botAnswer->sendMyProfileData(
+            $this->parser->id_telegram,
+            $this->parser->username,
+            $this->cryptoApi->getBtcCurrency()->btcPrice,
+            $this->cryptoApi->getEthCurrency()->ethPrice,
+            $this->cryptoApi->getUSDTCurrency()->usdtPrice
+        );
+    }
+
+    public function search(): void
+    {
+        $this->botAnswer->askUserIdToSearch();
+    }
+
+    public function getMyDeals(): void
+    {
+        $this->botAnswer->activeDeals();
+    }
+
+    public function support(): void
+    {
+        $this->botAnswer->explainHowToUseBot();
+    }
+
+    public function unknownCommand(): void
+    {
+        $this->botAnswer->unknownCommand();
+    }
 
 }
