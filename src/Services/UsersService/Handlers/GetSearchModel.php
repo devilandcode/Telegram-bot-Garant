30195c89f55a9f8e2d049551fde15d008e8acba1
devilandcode <devilandcode@proton.me>
2023-12-07 16:10:18 +0300

    Add GetSearchModel Handler

---
+++ src/Services/UsersService/Handlers/GetSearchModel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services\UsersService\Handlers;
+
+use App\Kernel\Config\ConfigInterface;
+use App\Kernel\HTTP\BotapiInterface;
+use App\Models\Search;
+use App\Services\UsersService\Repositories\UserRepository;
+
+class GetSearchModel
+{
+    public function get(BotapiInterface $botapi, UserRepository $repository, ConfigInterface $config): Search
+    {
+        $telegramId = $botapi->phpInput()->message->from->id;
+        $getBuyersLastSearchedArray = $repository->showLastSearchData($telegramId);
+
+        /** Get Names of columns at search_history table*/
+        $id = $config->get('database.search_name_of_primary_key');
+        $idBuyer = $config->get('database.search_name_of_column_with_id_buyer');
+        $idSeller = $config->get('database.search_name_of_column_with_id_seller');
+        $amount = $config->get('database.search_name_of_column_with_crypto_amount');
+        $terms = $config->get('database.search_name_of_column_with_terms_of_deal');
+        $startTime = $config->get('database.search_name_of_column_with_start_search_time');
+
+        /** @var Search */
+        return new Search(
+            $getBuyersLastSearchedArray[$id],
+            $getBuyersLastSearchedArray[$idBuyer],
+            $getBuyersLastSearchedArray[$idSeller],
+            $getBuyersLastSearchedArray[$amount],
+            $getBuyersLastSearchedArray[$terms],
+            $getBuyersLastSearchedArray[$startTime]
+        );
+    }
+}
--- src/Services/UsersService/UserService.php
+++ src/Services/UsersService/UserService.php
@@ -7,6 +7,7 @@ use App\Kernel\HTTP\BotapiInterface;
 use App\Keyboards\Keyboards;
 use App\Messages\Messages;
 use App\Models\Search;
+use App\Services\UsersService\Handlers\GetSearchModel;
 use App\Services\UsersService\Handlers\IsTimeForCreateDealIsOver;
 use App\Services\UsersService\Handlers\GetCryptoCurrencyOfDeal;
 use App\Services\UsersService\Handlers\isKeywordExistInMessageFromBot;
@@ -64,16 +65,22 @@ class UserService
 
     public function handleAmmoutOfDeal(string $messageFromBot)
     {
+        $searchModel = $this->getSearchModel();
+
         $isTimeForCreateDealIsOver = (new IsTimeForCreateDealIsOver())
             ->check(
-                $this->getSearchModel(),
+                $searchModel,
                 $this->config
             );
 
         if ($isTimeForCreateDealIsOver) {
             $this->botMessages->showTimeIsOver();
         }else {
-            $cryptoCurrency = (new GetCryptoCurrencyOfDeal())->get($messageFromBot);
+            $amountOfDeal = trim($messageFromBot);
+            $this->repository->addCryptoAmountToSeacrhTable(
+                $amountOfDeal,
+                $searchModel->id()
+            );
         }
     }
 
@@ -85,26 +92,10 @@ class UserService
 
     private function getSearchModel(): Search
     {
-        $telegramId = $this->botApi->phpInput()->message->from->id;
-        $getBuyersLastSearchedArray = $this->repository->showLastSearchData($telegramId);
-
-        /** Get Names of columns at search_history table*/
-        $id = $this->config->get('database.search_name_of_primary_key');
-        $idBuyer = $this->config->get('database.search_name_of_column_with_id_buyer');
-        $idSeller = $this->config->get('database.search_name_of_column_with_id_seller');
-        $amount = $this->config->get('database.search_name_of_column_with_crypto_amount');
-        $terms = $this->config->get('database.search_name_of_column_with_terms_of_deal');
-        $startTime = $this->config->get('database.search_name_of_column_with_start_search_time');
-
-        /** @var Search */
-        return new Search(
-            $getBuyersLastSearchedArray[$id],
-            $getBuyersLastSearchedArray[$idBuyer],
-            $getBuyersLastSearchedArray[$idSeller],
-            $getBuyersLastSearchedArray[$amount],
-            $getBuyersLastSearchedArray[$terms],
-            $getBuyersLastSearchedArray[$startTime]
+        return (new GetSearchModel())->get(
+            $this->botApi,
+            $this->repository,
+            $this->config
         );
     }
-
 }
