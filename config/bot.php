44d6e2a9988bcbdaeaad23aca2514442cd313412
devilandcode <devilandcode@proton.me>
2023-12-15 21:33:11 +0300

    update config

--- config/bot.php
+++ config/bot.php
@@ -2,6 +2,6 @@
 
 
 return [
-  'token' => '5839395074:AAH3zewbl5e4T0tpvrjjS5TVsXekV2PTCAg',
-  'admin_chat_id' => '-1002037545977',
+    'token' => '5839395074:AAH3zewbl5e4T0tpvrjjS5TVsXekV2PTCAg',
+    'admin_chat_id' => '-100203754523477',
 ];
